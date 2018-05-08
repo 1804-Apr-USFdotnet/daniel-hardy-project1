29ed54ad6bc743d11dbee1643d57a950f6b43e8f
Daniel Hardy <danielhardy302@gmail.com>
2018-05-08 13:17:36 -0400

    Crud operations work.  Test are not correctly written, need more test

---
+++ Project1_Refractored/ClientWebApp/UnitTestProjectBusiness/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjectBusiness")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjectBusiness")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c6d7b6fe-2c21-4e48-91d3-a78a3bc9bd30")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Project1_Refractored/ClientWebApp/UnitTestProjectBusiness/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace UnitTestProjectBusiness
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Models.Resturant r = new Models.Resturant()
+        {
+            Name = "Test",
+            Address = "Test",
+            City = "Test",
+            State = "tS",
+            FoodType = "Test"
+        };
+        [TestMethod]
+        public void TestAddResturant()
+        {
+            BusinessLayer.BusinessLibrary bl = new BusinessLayer.BusinessLibrary();
+            bl.addResturant(r);
+            List<Models.Resturant> list = bl.getAllResturants();
+
+            Assert.AreEqual(r.Name, list[list.Count - 1].Name);
+        }
+
+        [TestMethod]
+        public void TestUpdateResturant()
+        {
+            BusinessLayer.BusinessLibrary bl = new BusinessLayer.BusinessLibrary();
+            r.Name = "New";
+            bl.updateResturant(r);
+            List<Models.Resturant> list = bl.getAllResturants();
+
+            Assert.AreEqual(r.Name, list[list.Count - 1].Name);
+        }
+
+        [TestMethod]
+        public void DeleteResturant()
+        {
+            BusinessLayer.BusinessLibrary bl = new BusinessLayer.BusinessLibrary();
+            r.Name = "New";
+            //bl.updateResturant(r);
+            List<Models.Resturant> list = bl.getAllResturants();
+            int x = list.Count();
+            bl.deleteResturant(r);
+            List<Models.Resturant> list1 = bl.getAllResturants();
+            Assert.AreNotEqual(x,list1.Count);
+        }
+    }
+}
